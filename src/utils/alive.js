9e9f2b4f609f44682a841bf78f5655f3e7269d00
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-04-27 17:17:15 +0100

    server ping added

--- src/utils/alive.js
+++ src/utils/alive.js
@@ -1,6 +1,7 @@
 import https from "https";
 
-export function keepAlive(url) {
+export function keepAlive() {
+  const url = "https://defcomm-app-server.onrender.com/health/ready";
   const interval = 14 * 60 * 1000; // 14 minutes (Render sleeps at 15)
 
   setInterval(() => {
