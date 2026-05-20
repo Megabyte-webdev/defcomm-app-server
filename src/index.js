81ae34335bf11c0aac3549971521b712f83292e8
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-05-20 11:01:15 +0100

    recent update

--- src/index.js
+++ src/index.js
@@ -148,4 +148,12 @@ app.use((req, res) => {
 //   // });
 // }
 
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("UNHANDLED REJECTION:", reason);
+});
+
 export default app;
